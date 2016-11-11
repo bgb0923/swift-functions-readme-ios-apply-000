3927d0d4d09cd493dc840bb457769cb520545716
William Brancato <bgb0923@Mac-mini.local>
2016-11-11 17:11:02 -0500

    Modified Playground File

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,44 +1,44 @@
 func sayHello() {
     let greeting = "Hello there!"
-    print(greeting)
+    print (greeting)
 }
 
 sayHello()
+// Prints "Hello there!"
+
 sayHello()
+// Prints "Hello there!"
 
-// Uncomment this line to see the error that is printed in the console.
-// (You can uncomment a line by removing the // at the beginning of the line.)
-//print(greeting)
+// print(greeting)
 
-
-func sayHelloToGarfield() {
-    print("Hello, Garfield, why do you sleep so much?")
+func sayGoodbye() {
+    let farewell = "Goodbye, World"
+    print (farewell)
 }
 
-func sayHelloToBuster() {
-    print("Hello, Buster, why do you sleep so much?")
+sayGoodbye()
+// Prints "Goodbye, world."
+
+//print(farewell)
+
+func sayHelloToGarfield(){
+    print ("Hello, Garfield, why do you sleep so much?")
 }
 
-func sayHelloToScratchy() {
-    print("Hello, Scratchy, why do you sleep so much?")
+func sayHelloToBuster(){
+    print ("Hello, Buster, why do you sleep so much?")
 }
 
-
-func sayHelloToCat(catName: String) {
-    print("Hello, \(catName), why do you sleep so much?")
+func sayHelloToScratchy(){
+    print ("Hello, Scratchy, why do you sleep so much?")
 }
 
-
-func sayHello(name: String) {
+func sayHello(name: String){
     print("Hello \(name), why do you sleep so much?")
 }
 
 sayHello(name: "Mittens")
 // Prints "Hello Mittens, why do you sleep so much?"
 
-sayHello(name: "Socks")
-// Prints "Hello Socks, why do you sleep so much?"
-
 let dumpsterCat = "Rocky"
 sayHello(name: dumpsterCat)
-// Prints "Hello Rocky, why do you sleep so much?"
